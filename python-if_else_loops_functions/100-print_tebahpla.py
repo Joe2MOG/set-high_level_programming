a6f5dc5d8a36f7569203559546ae2febbed7f876
Joe2MOG <albertjoseph08066227697@gmail.com>
2026-07-13 00:46:48 +0100

    Fix pycodestyle whitespace issues across all files

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -4,4 +4,3 @@ for i in range(25, -1, -1):
         print("{:c}".format(97 + i), end="")
     else:
         print("{:c}".format(65 + i), end="")
-        
--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -6,4 +6,3 @@ def uppercase(str):
         else:
             print("{:c}".format(ord(c)), end="")
     print("")
-    
