4d9dbec04b2aac62c7757cfd9ae35d987b55f652
Joe2MOG <albertjoseph08066227697@gmail.com>
2026-07-13 00:52:12 +0100

    Fix print call count and line length issues

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def uppercase(str):
     for c in str:
-        if 97 <= ord(c) <= 122:
-            print("{:c}".format(ord(c) - 32), end="")
-        else:
-            print("{:c}".format(ord(c)), end="")
+        offset = 32 if 97 <= ord(c) <= 122 else 0
+        print("{:c}".format(ord(c) - offset), end="")
     print("")
