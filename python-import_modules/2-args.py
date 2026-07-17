4af589e0f2e8790c421a18a5dd08783ef2806029
Joe2MOG <albertjoseph08066227697@gmail.com>
2026-07-17 01:24:24 +0100

    Fix pycodestyle whitespace across import_modules project

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -12,4 +12,3 @@ if __name__ == "__main__":
         print("{} arguments:".format(n))
     for i in range(1, len(argv)):
         print("{}: {}".format(i, argv[i]))
-        
--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -7,4 +7,3 @@ if __name__ == "__main__":
     for arg in argv[1:]:
         total += int(arg)
     print(total)
-    
