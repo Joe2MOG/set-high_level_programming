eef5323e36dc76bb10b4dadc48974cb8042ae84f
Joe2MOG <albertjoseph08066227697@gmail.com>
2026-07-17 01:05:19 +0100

    python-import_modules project 1-5

---
+++ python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Prints all names defined in the hidden_4 module, alphabetically"""
+import hidden_4
+
+if __name__ == "__main__":
+    names = [name for name in dir(hidden_4) if not name.startswith("__")]
+    for name in sorted(names):
+        print(name)
+        
