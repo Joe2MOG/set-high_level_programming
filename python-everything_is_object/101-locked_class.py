2a7bd12aad2e7562420569efa2abee2e2e29007d
Joe2MOG <albertjoseph08066227697@gmail.com>
2026-07-25 17:37:35 +0100

    Fix tasks 9, 19, 30, 34: correct answers and pycodestyle

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
+"""Module that provides a LockedClass with restricted attributes."""
+
+
 class LockedClass:
+    """A class that only allows the 'first_name' attribute."""
     __slots__ = ("first_name",)
