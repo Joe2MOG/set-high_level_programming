6214af3e3ff867f34f65635f622c4725713e6413
Joe2MOG <albertjoseph08066227697@gmail.com>
2026-08-13 00:21:52 +0100

    Complete Object Relational Mapping project (tasks 0-17)

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Display states matching user input (safe from SQL injection)."""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306
+    )
+    cur = db.cursor()
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    cur.execute(query, (sys.argv[4],))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
